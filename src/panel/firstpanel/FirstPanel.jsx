5257d1b0120176e1b7b370080dba8a6339a189c9
zhu <zrcccrz@163.com>
2020-02-12 00:12:52 +0800

    修改基本样式

---
+++ src/panel/firstpanel/FirstPanel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class FirstPanel extends React.Component {
+
+    render() {
+        return (
+            <div>Panel1</div>
+        )
+    }
+}
+
+export default FirstPanel;
---
+++ src/panel/secondpanel/SecondPanel.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../component/header/Header';
+
+class SecondPanel extends React.Component {
+
+    render() {
+        return (
+            <Header title="Overview"></Header>
+        )
+    }
+}
+
+export default SecondPanel;
