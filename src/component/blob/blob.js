5b33c400283f9a6f88bc6db135c2bbc9bf717228
zhu <zrcccrz@163.com>
2020-03-29 18:08:16 +0800

    venn

--- src/component/blob/blob.js
+++ src/component/blob/blob.js
@@ -3,7 +3,9 @@ import * as d3 from 'd3'
 import { createBlob } from '../../util/path'
 import Axis from './Axis';
 import { connect } from 'react-redux';
+import * as venn from 'venn.js';
 import { setCountedLayer } from '../../actions/data';
+import { createMask } from '../../util/tools';
 
 // the width of svg is 2*BOX_WIDTH
 const BOX_WIDTH = 250;
@@ -88,6 +90,7 @@ class Blobs extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      sets: [],
       // TODO get the number of the group
       // blobs: [1000, 500, 200], 
       // TODO 获得blob的层数
@@ -108,6 +111,7 @@ class Blobs extends React.Component {
     }
 
     this.$slider = React.createRef();
+    this.$venn = React.createRef();
     this.$handle = React.createRef();
   }
 
@@ -117,14 +121,98 @@ class Blobs extends React.Component {
 
   componentDidUpdate(prevProps) {
     if(this.props.layer !== prevProps.layer) {
+      // 更新layer
       let {rangeScale} = this.state;
       rangeScale.domain([0, this.props.layer]);
+
+      
       this.setState({
         rangeScale,
         layers: this.props.layer
       })
+
       this.initDrag();
     }
+
+    if(JSON.stringify(this.props.group) !== JSON.stringify(prevProps.group)) {
+      // 更新韦恩图
+      let sets = [];
+      let {group} = this.props;
+
+      // TODO 暂时只考虑32步以内
+
+      let groupKeys = Object.keys(group);
+      // 掩码数组的长度: 每一位对应Object.keys()得到的person_id数组集合是否包含该元素， 包含则为true
+      let boolSize = groupKeys.length;
+      // 映射每一个person_id的掩码数组的状态
+      let count = {};
+      // 每个key对应掩码数组的下标
+      let keyIndex = 0;
+      for(let key in group) {
+        let person_ids = Object.keys(group[key]);
+        sets.push({
+          sets: [key],
+          size: person_ids.length
+        })
+
+        // eslint-disable-next-line 
+        person_ids.forEach(id => {
+          if(count[id] === undefined) {
+            count[id] = new Array(boolSize).fill(false);
+          }
+          count[id][keyIndex] = true;
+        })
+
+        keyIndex++;
+      }
+
+       // 各元素掩码的集合 - mask: 元素的个数 
+      let maskCount = {}
+      for(let id in count) {
+        let mask = createMask(...count[id]);
+        if(maskCount[mask] === undefined) {
+          maskCount[mask] = 0;
+        }
+        maskCount[mask]++;
+      }
+
+      for(let mask in maskCount) {
+        let maskString = Number(mask).toString(2);
+        let _sets = [];
+        for(let i=maskString.length-1; i>=0; i--) {
+          if(maskString[i] === '1') {
+            _sets.push(groupKeys[i]);
+          }
+        }
+        if(_sets.length!==1) {
+          sets.push({
+            sets: _sets,
+            size: maskCount[mask]
+          })
+        }
+      }
+      
+      // let result = data.reduce((a, b) => a.filter(c => b.includes(c)));
+      this.setState({
+        sets
+      })
+      this.initVene(sets)
+    }
+  }
+
+  initVene(sets) {
+    try{
+      d3.select(this.$venn.current).datum(
+        sets
+      ).call(
+        venn.VennDiagram()
+          .width(500)
+          .height(500)
+      )
+    }catch{
+      console.log(sets);
+    }
+   
   }
 
   getRadius(r) {
@@ -192,8 +280,8 @@ class Blobs extends React.Component {
   }
 
   render() {
-    let { handlePos,rangeScale} = this.state;
-    let { blobs,layer } = this.props;
+    let { handlePos,rangeScale,sets} = this.state;
+    let { layer } = this.props;
 
     return (
       <svg width="100%" height="100%" viewBox={`0 0 ${2 * BOX_WIDTH} ${2 * BOX_WIDTH}`} xmlns="http://www.w3.org/2000/svg">
@@ -204,12 +292,15 @@ class Blobs extends React.Component {
           </linearGradient>
         </defs>
         
-        {
+        {/* {
           blobs.map((r, i) => (
             r !== undefined && <Blob key={`blob-${i}`} radius={this.getRadius(r)} />
           ))
-        }
+        } */}
 
+        <foreignObject x="0" y="0" width="500" height="500" ref={this.$venn}>
+        </foreignObject>
+        
         <g transform={`translate(${BOX_WIDTH},${BOX_WIDTH})`}>
           {
             fakeData.map((data,i) => {
@@ -219,7 +310,7 @@ class Blobs extends React.Component {
         </g>
 
         {
-          blobs.length>0 && 
+          sets.length>0 && 
           <g className="slider" ref={this.$slider}>
             <rect
               className="slider-overlay"
@@ -246,8 +337,13 @@ class Blobs extends React.Component {
 }
 
 const mapStateToProps = (state) => {
+  let _group = {};
+  for(let key in state.group) {
+    _group[key] = state.group[key]['people']
+  }
   return {
-    layer: state.step
+    layer: state.step,
+    group: _group
   }
 }
 
