5efbcb0e0fbf997310e8765675f8974e4307bd7b
zhu <zrcccrz@163.com>
2020-04-21 22:00:05 +0800

    按钮

--- src/component/dimension/DimensionFilter.js
+++ src/component/dimension/DimensionFilter.js
@@ -228,7 +228,7 @@ export function DimensionFilter({ _width, _height, _margin,  peopleOfGroup, sele
                 />
                 <rect width="100%" height="100%" fill="transparent"></rect>
             </g>
-            <foreignObject x="-128" y="-3px" width="160px" height="50" >
+            <foreignObject x="-80" y="-3px" width="220px" height="50" >
                 <div className="dimension-btn-container">
                     <CircleBtn type={4} onClick={clear}></CircleBtn>
                     <CircleBtn type={5} onClick={clear}></CircleBtn>
