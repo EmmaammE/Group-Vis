64b115b642d0fef77afcaf8f9b9b5c3b59bba26f
zhu <zrcccrz@163.com>
2020-06-22 23:16:56 +0800

    修改map的刷选

--- src/component/map/map.js
+++ src/component/map/map.js
@@ -18,16 +18,7 @@ const path2 = d3.geoPath()
     .scale(250)
     .translate([465, 365]));
 
-let startLoc = [];
-let brushFlag = false;
-let brushWidth;
-let brushHeight;
-let brushPersonsId;
-let svgWidth, svgHeight;
-let svgRatio
-let drawData
-let prevPropsData
-
+let brush = d3.brush();
 class Map extends React.Component {
   constructor(props) {
     super(props);
@@ -49,12 +40,6 @@ class Map extends React.Component {
         .range([5, 5]).domain([1, 999])
         .clamp(true),
       $d: '',
-      brushVisibility: "hidden",
-      brushTransX: 0,
-      brushTransY: 0,
-      brushWidth: 0,
-      brushHeight: 0,
-
     };
     this.projection = d3.geoMercator()
       .center([110, 31])
@@ -68,17 +53,15 @@ class Map extends React.Component {
     this.$tooltip = React.createRef();
     this.showTooltip = this.showTooltip.bind(this);
 
-
-    this.handleBrushMouseDown = this.handleBrushMouseDown.bind(this)
-    this.handleBrushMouseMove = this.handleBrushMouseMove.bind(this)
-    this.handleBrushMouseUp = this.handleBrushMouseUp.bind(this)
-
     this.showTooltip = this.showTooltip.bind(this)
     this.showTooltip2 = this.showTooltip2.bind(this)
     this.handleClickX = this.handleClickX.bind(this)
     this.handleMouseOut = this.handleMouseOut.bind(this)
     this.handleClear = this.handleClear.bind(this)
 
+    this.brushing = this.brushing.bind(this);
+    this.brushEnd = this.brushEnd.bind(this);
+    this.brushStart = this.brushStart.bind(this);
   }
 
   componentDidMount() {
@@ -93,30 +76,37 @@ class Map extends React.Component {
     //               .attr("transform",transform);
     //             },100)(d3.event.transform)
     //           }))
-    let container = this.$container.current
-    // console.log("container",container.clientWidth,container.clientHeight)
-    svgWidth = container.clientWidth
-    svgHeight = container.clientHeight
-    svgRatio = svgWidth / (BOX_WIDTH * 2)
+
+   
+
+    let container = this.$container.current;
+    d3.select(container)
+      .call(brush
+        .on("start", this.brushStart)
+        .on("brush", this.brushing)
+        .on("end", this.brushEnd));
   }
 
-  // componentDidUpdate(prevProps) {
-  //   let { pos2sentence, addr } = this.props;
-  //   if (JSON.stringify(pos2sentence) !== JSON.stringify(prevProps.pos2sentence)) {
-  //     let { rangeScale } = this.state;
-  //     if(addr) {
-  //       let extent = d3.extent(Object.keys(pos2sentence).filter(e => addr[e]).map(e => pos2sentence[e].length))
-  //       console.log(extent)
-  //       if(extent[0] !== undefined) {
-  //         rangeScale.range([5, 12]).domain(extent)
-  //       }
-  //     }
+  componentDidUpdate(prevProps) {
+    let { sentence2pos, pos2sentence, addr } = this.props;
+    
+    if (JSON.stringify(pos2sentence) !== JSON.stringify(prevProps.pos2sentence)) {
+      let drawData = figureDrawData(addr, pos2sentence, this.projection, sentence2pos)
+    //   let { rangeScale } = this.state;
+    //   if(addr) {
+    //     let extent = d3.extent(Object.keys(pos2sentence).filter(e => addr[e]).map(e => pos2sentence[e].length))
+    //     console.log(extent)
+    //     if(extent[0] !== undefined) {
+    //       rangeScale.range([5, 12]).domain(extent)
+    //     }
+    //   }
 
-  //     this.setState({
-  //       rangeScale
-  //     })
-  //   }
-  // }
+      this.setState({
+        // rangeScale
+        drawData
+      })
+    }
+  }
 
   handleClickX() {
     this.setState({
@@ -137,6 +127,7 @@ class Map extends React.Component {
   }
 
   showTooltip(e) {
+    let {drawData} = this.state;
     if (!this.state.tooltip.show) {
       let index = e.target.getAttribute("index")
       this.setState({
@@ -158,6 +149,7 @@ class Map extends React.Component {
   }
 
   showTooltip2(e) {
+    let {drawData} = this.state;
     let index = e.target.getAttribute("index")
     this.setState({
       tooltip: {
@@ -198,107 +190,74 @@ class Map extends React.Component {
     })
   }
 
-  // 下面三个函数为刷选框的监听函数
-  handleBrushMouseDown(v) {
-    // console.log("getbox",v.nativeEvent.offsetX,v.nativeEvent.offsetY)
-    startLoc = [v.nativeEvent.offsetX / svgRatio, v.nativeEvent.offsetY / svgRatio]
-    brushFlag = true
-    this.setState({
-      brushTransX: startLoc[0],
-      brushTransY: startLoc[1],
-      brushVisibility: "visible"
-    })
+  brushStart() {
+    let {drawData} = this.state;
+    if (d3.event.sourceEvent.type === "end") return;
+    // 清空之前刷选的
+    drawData.forEach(d => { d.isChoose = false});
   }
-  handleBrushMouseMove(v) {
-    if (brushFlag) {
-      let nowX = v.nativeEvent.offsetX / svgRatio
-      let nowY = v.nativeEvent.offsetY / svgRatio
-      brushWidth = nowX - startLoc[0]
-      brushHeight = nowY - startLoc[1]
-      if (brushWidth < 0) {
-        nowX = startLoc[0] + brushWidth
-        brushWidth = Math.abs(brushWidth)
-      } else {
-        nowX = startLoc[0]
-      }
-      if (brushHeight < 0) {
-        nowY = startLoc[1] + brushHeight
-        brushHeight = Math.abs(brushHeight)
-      } else {
-        nowY = startLoc[1]
-      }
-      this.setState({
-        brushTransX: nowX,
-        brushTransY: nowY,
-        brushWidth: brushWidth,
-        brushHeight: brushHeight
-      })
-    }
+
+  brushing() {
+    // let { drawData } = this.state;
+    // 1if (d3.event.selection) {
+    //   const [[x0, y0], [x1, y1]] = d3.event.selection;
+    //   drawData.forEach(d => {
+    //     d.isChoose =
+    //       x0 <= d.xy[0] - d.r && d.xy[0] + d.r < x1 && y0 <= d.xy[1] - d.r && d.xy[1] + d.r < y1;
+    //   });
+
+    //   this.setState({
+    //     drawData
+    //   })
+    // }
   }
-  handleBrushMouseUp(v) {
-    let x1 = this.state.brushTransX
-    let y1 = this.state.brushTransY
-    let x2 = x1 + this.state.brushWidth
-    let y2 = y1 + this.state.brushHeight
-    // 拿到尺寸数据反推回去
-    // console.log("x-----y",x1,y1,x2,y2)
-    // console.log("drawData",drawData)
-    let clearAll = true
-    let people = {};
 
-    drawData.forEach(d => {
-      if (d.xy[0] >= x1 && d.xy[0] <= x2 && d.xy[1] >= y1 && d.xy[1] <= y2) {
-        d.isChoose = true
-        clearAll = false
-      }
+  brushEnd() {
+    if (!d3.event.sourceEvent || d3.event.sourceEvent.type === "end") return;
+    d3.select(this.$container.current).call(brush.clear);
+    
+    let { drawData } = this.state;
+    if (d3.event.selection) {
+      let people = {};
+      const [[x0, y0], [x1, y1]] = d3.event.selection;
 
-      if (d.isChoose === true) {
-        d['people'] && d['people'].forEach(p => {
-          p.forEach(id => {
-            people[id] = true
-          })
-        })
-      }
-    })
-
-    if (clearAll) {
       drawData.forEach(d => {
-        d.isChoose = false
+        d.isChoose =
+          x0 <= d.xy[0] - d.r && d.xy[0] + d.r < x1 && y0 <= d.xy[1] - d.r && d.xy[1] + d.r < y1;
+        if (d.isChoose === true) {
+          d['people'] && d['people'].forEach(p => {
+            p.forEach(id => {
+              people[id] = true
+            })
+          })
+        }
       })
-      people = {}
-    }
 
-    // console.log(people)
-    this.props.setPerson(people)
-    brushFlag = false
-    this.setState({
-      brushVisibility: "hidden",
-      brushTransX: 0,
-      brushTransY: 0,
-      brushWidth: 0,
-      brushHeight: 0
-    })
+      this.props.setPerson(people)
+      this.setState({
+        drawData
+      })
+    }
+    
+    
   }
+
 
   handleMouseOut() {
     if (!this.state.tooltip.data.tipHasX) {
       this.setState({ tooltip: { show: false } })
     }
-
   }
 
   handleClear() {
+    let { drawData } = this.state;
     drawData.forEach(d => {
       d.isChoose = false
     })
-    this.setState({
-      brushVisibility: "hidden",
-      brushTransX: 0,
-      brushTransY: 0,
-      brushWidth: 0,
-      brushHeight: 0
-    })
     this.props.setPerson({})
+    this.setState({
+      drawData
+    })
   }
 
   render() {
@@ -306,13 +265,12 @@ class Map extends React.Component {
       projection = this.projection;
 
     let { addr, sentence2pos, pos2sentence } = this.props;
-    let { tooltip, $d } = this.state;
+    let { tooltip, $d, drawData } = this.state;
 
-    if (JSON.stringify(prevPropsData) !== JSON.stringify(this.props)) {
-      prevPropsData = this.props;
-      drawData = figureDrawData(addr, pos2sentence, projection, sentence2pos)
+    // if (JSON.stringify(prevPropsData) !== JSON.stringify(this.props)) {
+    //   prevPropsData = this.props;
       // console.log(drawData)
-    }
+    // }
     return (
       <>
         {tooltip.show && <Tip {...tooltip.data} handleClickX={this.handleClickX} />}
@@ -333,18 +291,15 @@ class Map extends React.Component {
           <CircleBtn type={6} active={true} />
         </div>
 
-        <svg viewBox={`0 0 ${2 * BOX_WIDTH} ${2 * BOX_HEIGHT}`} xmlns="http://www.w3.org/2000/svg"
+               <svg viewBox={`0 0 ${2 * BOX_WIDTH} ${2 * BOX_HEIGHT}`} xmlns="http://www.w3.org/2000/svg"
           style={{ position: 'relative' }}
-          ref={this.$container}
-          onMouseDown={this.handleBrushMouseDown}
-          onMouseMove={this.handleBrushMouseMove}
-          onMouseUp={this.handleBrushMouseUp}
         // preserveAspectRatio="xMinYMin"
         >
           <clipPath id="myClip">
             <rect x="450" y="300" width="70" height="110" stroke="black" fill="transparent" />
           </clipPath>
           <path d={$d} stroke="white" />
+          
           <g ref={this.$map}>
             <g>
               {china.features.map((d, i) => (
@@ -356,15 +311,19 @@ class Map extends React.Component {
                 />
               ))}
             </g>
+            
+            <g
+            ref={this.$container}
+          ></g>
             <g>
               {
                 addr &&
-                drawData.map((d, i) => (
+                drawData && drawData.map((d, i) => (
                   <circle key={'cir-' + i}
                     index={i}
                     r={d.r}
                     fill="#277077"
-                    stroke={d.isChoose ? '#898989' : '#277077'}
+                    stroke={d.isChoose ? '#111' : '#277077'}
                     fillOpacity={0.2}
                     onMouseOver={this.showTooltip}
                     onMouseOut={this.handleMouseOut}
@@ -374,22 +333,6 @@ class Map extends React.Component {
               }
             </g>
           </g>
-          {/* 绘制刷选框 */}
-          <g
-            transform={`translate(${this.state.brushTransX},${this.state.brushTransY})`}
-            visibility={this.state.brushVisibility}
-          >
-            <rect
-              className="brush"
-              width={this.state.brushWidth}
-              height={this.state.brushHeight}
-              opacity="0.3"
-              strokeWidth="1.5"
-              stroke="black"
-            >
-            </rect>
-          </g>
-
           <rect x="450" y="300" width="70" height="110" stroke="black" fill="transparent" />
           <g clipPath="url(#myClip)">
             {china.features.map((d, i) => (
